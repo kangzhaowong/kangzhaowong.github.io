f2727eb8d2c2c18da7f3c01437a4aa4b9eb18d3c
kangzhaowong <117423228+kangzhaowong@users.noreply.github.com>
2024-06-21 18:02:31 +0800

    update

---
+++ scripts.js
@@ -0,0 +1,19 @@
+function eventListeners() {
+    var flkty = new Flickity(".main-carousel");
+    document.getElementsByClassName("flickity-viewport")[0].style.height =
+      "";
+    flkty.resize();
+    flkty.reposition();
+    window.addEventListener("resize", (event) => {
+      document.getElementsByClassName("flickity-viewport")[0].style.height =
+        "";
+      flkty.resize();
+      flkty.reposition();
+    });
+    flkty.on("change", function (index) {
+      flkty.reposition();
+    });
+  }
+  document.addEventListener("DOMContentLoaded", () => {
+    eventListeners();
+  });
