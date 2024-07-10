16dc994ec5fd8295546ef9877c208969756a444c
kangzhaowong <117423228+kangzhaowong@users.noreply.github.com>
2024-07-10 13:53:04 +0800

    update

--- scripts.js
+++ scripts.js
@@ -1,19 +1,22 @@
+let lastScroll = 0
+
 function eventListeners() {
-    var flkty = new Flickity(".main-carousel");
+  var flkty = new Flickity(".main-carousel");
+  document.getElementsByClassName("flickity-viewport")[0].style.height =
+    "";
+  flkty.resize();
+  flkty.reposition();
+  window.addEventListener("resize", (event) => {
     document.getElementsByClassName("flickity-viewport")[0].style.height =
       "";
     flkty.resize();
     flkty.reposition();
-    window.addEventListener("resize", (event) => {
-      document.getElementsByClassName("flickity-viewport")[0].style.height =
-        "";
-      flkty.resize();
-      flkty.reposition();
-    });
-    flkty.on("change", function (index) {
-      flkty.reposition();
-    });
-  }
-  document.addEventListener("DOMContentLoaded", () => {
-    eventListeners();
   });
+  flkty.on("change", function (index) {
+    flkty.reposition();
+  });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  eventListeners();
+});
